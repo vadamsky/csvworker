42c7a905be990f399ec438fcb3a1cb0a2fe86157
vadamsky <vadamsky@users.noreply.github.com>
2017-08-04 17:30:37 +0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
-    //
+    // Load file "example.csv"
     CsvWorker csv;
     csv.loadFromFile("example.csv");
-    cout << csv.getRowsNumber() << "  " << csv.getColumnsNumber() << endl;
-
+    // view rows and columns counts from it
+    cout << csv.getRowsCount() << "  " << csv.getColumnsCount() << endl;
+    // change part of reading data
     csv.getFieldRef(0, 2) = "0";
     csv.getFieldRef(1, 1) = "0";
     csv.getFieldRef(1, 3) = "0";
@@ -18,22 +19,22 @@ int main()
     csv.getFieldRef(3, 1) = "0";
     csv.getFieldRef(3, 3) = "0";
     csv.getFieldRef(4, 2) = "0";
-
-    for(unsigned int i=0;i<csv.getRowsNumber();++i)
+    // view changed data
+    for(unsigned int i = 0; i < csv.getRowsCount(); ++i)
     {
-        //cout << csv.getRow(i) << endl;
-        for(unsigned int j=0;j<csv.getColumnsNumber();++j)
+        cout << csv.getRow(i) << endl;
+        for(unsigned int j = 0; j < csv.getColumnsCount(); ++j)
         {
-            cout << csv.getField(i, j) << ".";
+            cout << csv.getField(i, j) << ",";
         }
         cout << endl;
     }
-
+    // save changed data in "test.csv"
     csv.saveToFile("test.csv");
 
-    //
+    // Create new class instance (4*4)
     CsvWorker csv2(4,4);
-
+    // Fill data of this instance
     csv2.getFieldRef(0, 0) = "a";
     csv2.getFieldRef(0, 1) = "b";
     csv2.getFieldRef(0, 2) = "r";
@@ -45,7 +46,7 @@ int main()
     csv2.getFieldRef(2, 1) = "b";
     csv2.getFieldRef(2, 2) = "r";
     csv2.getFieldRef(2, 3) = "a";
-
+    // save data in "test2.csv"
     csv2.saveToFile("test2.csv");
 
     return 0;
